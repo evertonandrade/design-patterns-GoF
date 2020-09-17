f6dd96201c9d75b537dd50ba91173a220d69be8b
Everton Andrade <evertonfa7@gmail.com>
2020-09-17 10:45:13 -0300

    up adapter

--- structural/Adapter/Fabricante.cs
+++
@@ -1,9 +0,0 @@
-namespace Adapter
-{
-    public class Fabricante
-    {
-        public string Nome { get; set; }
-        public string Cidade { get; set; }
-        public int Ano { get; set; }
-    }
-}
